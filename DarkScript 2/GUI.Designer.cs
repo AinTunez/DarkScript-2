f174c69ea6b94eb28a31bf2d4c758478ff6b05ff
Cliff <cweinstein.tech@gmail.com>
2019-02-11 22:35:07 -0500

    fixes for kata

---
+++ DarkScript 2/GUI.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace DarkScript_2
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI));
+            this.editorPanel = new System.Windows.Forms.Panel();
+            this.editorSplit = new System.Windows.Forms.SplitContainer();
+            this.editorNumeric = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.editorVerbose = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openEMEVDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveEMEVDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveEMEVDAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editorPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.editorSplit)).BeginInit();
+            this.editorSplit.Panel1.SuspendLayout();
+            this.editorSplit.Panel2.SuspendLayout();
+            this.editorSplit.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.editorNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.editorVerbose)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // editorPanel
+            // 
+            this.editorPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.editorPanel.Controls.Add(this.editorSplit);
+            this.editorPanel.Location = new System.Drawing.Point(13, 46);
+            this.editorPanel.Name = "editorPanel";
+            this.editorPanel.Size = new System.Drawing.Size(1213, 697);
+            this.editorPanel.TabIndex = 0;
+            // 
+            // editorSplit
+            // 
+            this.editorSplit.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.editorSplit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.editorSplit.Location = new System.Drawing.Point(0, 0);
+            this.editorSplit.Name = "editorSplit";
+            // 
+            // editorSplit.Panel1
+            // 
+            this.editorSplit.Panel1.Controls.Add(this.editorNumeric);
+            // 
+            // editorSplit.Panel2
+            // 
+            this.editorSplit.Panel2.Controls.Add(this.editorVerbose);
+            this.editorSplit.Size = new System.Drawing.Size(1213, 697);
+            this.editorSplit.SplitterDistance = 403;
+            this.editorSplit.SplitterWidth = 20;
+            this.editorSplit.TabIndex = 0;
+            // 
+            // editorNumeric
+            // 
+            this.editorNumeric.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.editorNumeric.AutoIndent = false;
+            this.editorNumeric.AutoIndentChars = false;
+            this.editorNumeric.AutoIndentExistingLines = false;
+            this.editorNumeric.AutoScrollMinSize = new System.Drawing.Size(27, 16);
+            this.editorNumeric.BackBrush = null;
+            this.editorNumeric.CharHeight = 16;
+            this.editorNumeric.CharWidth = 8;
+            this.editorNumeric.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.editorNumeric.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.editorNumeric.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.editorNumeric.Font = new System.Drawing.Font("Consolas", 8.25F);
+            this.editorNumeric.IsReplaceMode = false;
+            this.editorNumeric.Location = new System.Drawing.Point(0, 0);
+            this.editorNumeric.Name = "editorNumeric";
+            this.editorNumeric.Paddings = new System.Windows.Forms.Padding(0);
+            this.editorNumeric.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.editorNumeric.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("editorNumeric.ServiceColors")));
+            this.editorNumeric.Size = new System.Drawing.Size(399, 693);
+            this.editorNumeric.TabIndex = 0;
+            this.editorNumeric.Zoom = 100;
+            this.editorNumeric.SelectionChanged += new System.EventHandler(this.editorNumeric_SelectionChanged);
+            this.editorNumeric.SelectionChangedDelayed += new System.EventHandler(this.editorNumeric_SelectionChangedDelayed);
+            this.editorNumeric.Scroll += new System.Windows.Forms.ScrollEventHandler(this.editorNumeric_Scroll);
+            this.editorNumeric.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Box_KeyDown);
+            // 
+            // editorVerbose
+            // 
+            this.editorVerbose.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.editorVerbose.AutoIndent = false;
+            this.editorVerbose.AutoIndentChars = false;
+            this.editorVerbose.AutoIndentExistingLines = false;
+            this.editorVerbose.AutoScrollMinSize = new System.Drawing.Size(27, 16);
+            this.editorVerbose.BackBrush = null;
+            this.editorVerbose.CharHeight = 16;
+            this.editorVerbose.CharWidth = 8;
+            this.editorVerbose.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.editorVerbose.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.editorVerbose.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.editorVerbose.Font = new System.Drawing.Font("Consolas", 8.25F);
+            this.editorVerbose.IsReplaceMode = false;
+            this.editorVerbose.Location = new System.Drawing.Point(0, 0);
+            this.editorVerbose.Name = "editorVerbose";
+            this.editorVerbose.Paddings = new System.Windows.Forms.Padding(0);
+            this.editorVerbose.ReadOnly = true;
+            this.editorVerbose.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.editorVerbose.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("editorVerbose.ServiceColors")));
+            this.editorVerbose.Size = new System.Drawing.Size(786, 693);
+            this.editorVerbose.TabIndex = 0;
+            this.editorVerbose.Zoom = 100;
+            this.editorVerbose.SelectionChanged += new System.EventHandler(this.editorVerbose_SelectionChanged);
+            this.editorVerbose.SelectionChangedDelayed += new System.EventHandler(this.editorVerbose_SelectionChangedDelayed);
+            this.editorVerbose.Scroll += new System.Windows.Forms.ScrollEventHandler(this.editorVerbose_Scroll);
+            this.editorVerbose.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Box_KeyDown);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1238, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openEMEVDToolStripMenuItem,
+            this.saveEMEVDToolStripMenuItem,
+            this.saveEMEVDAsToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openEMEVDToolStripMenuItem
+            // 
+            this.openEMEVDToolStripMenuItem.Name = "openEMEVDToolStripMenuItem";
+            this.openEMEVDToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openEMEVDToolStripMenuItem.Size = new System.Drawing.Size(278, 26);
+            this.openEMEVDToolStripMenuItem.Text = "Open EMEVD";
+            this.openEMEVDToolStripMenuItem.Click += new System.EventHandler(this.openEMEVDToolStripMenuItem_Click);
+            // 
+            // saveEMEVDToolStripMenuItem
+            // 
+            this.saveEMEVDToolStripMenuItem.Name = "saveEMEVDToolStripMenuItem";
+            this.saveEMEVDToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveEMEVDToolStripMenuItem.Size = new System.Drawing.Size(278, 26);
+            this.saveEMEVDToolStripMenuItem.Text = "Save EMEVD";
+            this.saveEMEVDToolStripMenuItem.Click += new System.EventHandler(this.saveEMEVDToolStripMenuItem_Click);
+            // 
+            // saveEMEVDAsToolStripMenuItem
+            // 
+            this.saveEMEVDAsToolStripMenuItem.Name = "saveEMEVDAsToolStripMenuItem";
+            this.saveEMEVDAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.saveEMEVDAsToolStripMenuItem.Size = new System.Drawing.Size(278, 26);
+            this.saveEMEVDAsToolStripMenuItem.Text = "Save EMEVD As";
+            this.saveEMEVDAsToolStripMenuItem.Click += new System.EventHandler(this.saveEMEVDAsToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(278, 26);
+            this.quitToolStripMenuItem.Text = "Quit";
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1238, 755);
+            this.Controls.Add(this.editorPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "GUI";
+            this.Text = "DarkScript 2";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.GUI_Load);
+            this.editorPanel.ResumeLayout(false);
+            this.editorSplit.Panel1.ResumeLayout(false);
+            this.editorSplit.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.editorSplit)).EndInit();
+            this.editorSplit.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.editorNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.editorVerbose)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel editorPanel;
+        private System.Windows.Forms.SplitContainer editorSplit;
+        private FastColoredTextBoxNS.FastColoredTextBox editorNumeric;
+        private FastColoredTextBoxNS.FastColoredTextBox editorVerbose;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openEMEVDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveEMEVDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveEMEVDAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+    }
+}
+
---
+++ DarkScript 2/GUI.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using FastColoredTextBoxNS;
+
+namespace DarkScript_2
+{
+    public partial class GUI : Form
+    {
+        public EMEVD currentEMEVD;
+        public GUI thisForm;
+        public bool scrolling = false;
+
+        public GUI()
+        {
+            InitializeComponent();
+            editorSplit.SplitterDistance = 400;
+            thisForm = this;
+        }
+
+        public class EMEVD
+        {
+            public string FilePath { get; set; }
+            public string NumericText { get; private set; }
+            public string VerboseText { get; private set; }
+
+            public static EMEVD Read(string path)
+            {
+                var emevd = new EMEVD(path);
+                emevd.Refresh();
+                return emevd;
+            }
+
+            private EMEVD(string path) => FilePath = path;
+
+            public void Refresh()
+            {
+                NumericText = Numeric();
+                VerboseText = Verbose();
+            }
+
+            public void RefreshVerbose()
+            {
+                VerboseText = Verbose();
+            }
+
+            public void Save(string input)
+            {
+                try
+                {
+                    File.WriteAllText("tmp.txt", input);
+                    var p = new Process();
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    p.StartInfo.FileName = "EventScriptTool.exe";
+                    p.StartInfo.Arguments = "tmp.txt -p -o \"" + FilePath + "\"";
+                    p.Start();
+                    p.WaitForExit();
+                    p.Dispose();
+                    File.Delete("tmp.txt");
+                    Refresh();
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+
+            public void SaveAs(string path, string input)
+            {
+                FilePath = path;
+                Save(input);
+            }
+
+            private string ReadFile(string arguments)
+            {
+                var sb = new StringBuilder();
+                try
+                {
+                    var p = new Process();
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.RedirectStandardError = true;
+                    p.OutputDataReceived += (sender, args) => sb.AppendLine(args.Data);
+                    p.ErrorDataReceived += (sender, args) => sb.AppendLine(args.Data);
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    p.StartInfo.FileName = "EventScriptTool.exe";
+                    p.StartInfo.Arguments = arguments;
+                    p.Start();
+                    p.BeginOutputReadLine();
+                    p.BeginErrorReadLine();
+                    p.WaitForExit();
+
+                    ActiveForm.Cursor = Cursors.Default;
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                    return "";
+                }
+                return sb.ToString();
+
+            }
+
+            private string Numeric() => ReadFile("-n -s \"" + FilePath + "\"");
+
+            private string Verbose() => ReadFile("-v -s \"" + FilePath + "\"");
+        }
+
+
+        private void GUI_Load(object sender, EventArgs e)
+        {
+            editorSplit.SplitterWidth = 20;
+            editorSplit.SplitterDistance = 400;
+        }
+
+        private void openEMEVDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    currentEMEVD = EMEVD.Read(ofd.FileName);
+                    editorNumeric.Text = AdjustNumeric(currentEMEVD.NumericText);
+                    editorVerbose.Text = AdjustVerbose(currentEMEVD.VerboseText, currentEMEVD.NumericText);
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private string AdjustNumeric(string numeric) => numeric.Replace(Environment.NewLine + "    ^", " ^");
+
+        private string AdjustVerbose(string verbose, string numeric) => verbose.Replace(Environment.NewLine + "Parameters:", " | Parameters:");
+
+        private void saveEMEVDAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (currentEMEVD == null) return;
+            var sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                this.Cursor = Cursors.WaitCursor;
+                int cursorPos = editorNumeric.SelectionStart;
+                int scrollPos = editorNumeric.VerticalScroll.Value;
+                try
+                {
+                    currentEMEVD.SaveAs(sfd.FileName, editorNumeric.Text.Replace(" ^", Environment.NewLine + "    ^"));
+                    editorNumeric.Text = AdjustNumeric(currentEMEVD.NumericText);
+                    editorVerbose.Text = AdjustVerbose(currentEMEVD.VerboseText, currentEMEVD.NumericText);
+                    editorNumeric.SelectionStart = cursorPos;
+                    editorNumeric.VerticalScroll.Value = scrollPos;
+                    editorVerbose.VerticalScroll.Value = scrollPos;
+                    editorNumeric.VerticalScroll.Value = scrollPos;
+                    editorVerbose.VerticalScroll.Value = scrollPos;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                    this.Cursor = Cursors.Default;
+                }
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        private void saveEMEVDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (currentEMEVD == null) return;
+            int cursorPos = editorNumeric.SelectionStart;
+            int scrollPos = editorNumeric.VerticalScroll.Value;
+            try
+            {
+                Cursor = Cursors.WaitCursor;
+                currentEMEVD.Save(editorNumeric.Text.Replace(" ^", Environment.NewLine + "    ^"));
+                editorVerbose.Text = AdjustVerbose(currentEMEVD.VerboseText, currentEMEVD.NumericText);
+                editorNumeric.Text = AdjustNumeric(currentEMEVD.NumericText);
+                editorNumeric.VerticalScroll.Value = scrollPos;
+                editorVerbose.VerticalScroll.Value = scrollPos;
+                editorNumeric.VerticalScroll.Value = scrollPos;
+                editorVerbose.VerticalScroll.Value = scrollPos;
+                editorNumeric.SelectionStart = cursorPos;
+                Cursor = Cursors.Default;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        private void ScrollPosChanged(FastColoredTextBox target, FastColoredTextBox source)
+        {
+            if (scrolling) return;
+            scrolling = true;
+            if (source.VerticalScroll.Value <= target.VerticalScroll.Maximum)
+            {
+                int a = target.VerticalScroll.Maximum;
+                int b = source.VerticalScroll.Maximum;
+                int c = source.VerticalScroll.Value;
+                int val = Math.Min(Math.Min(a, b), c);
+                int checkCount = 0;
+                while (target.VerticalScroll.Value != val)
+                {
+                    if (checkCount > 2) break;
+                    target.VerticalScroll.Value = val;
+                    checkCount++;
+                }
+            }
+            Console.WriteLine("SRC --> " + source.VerticalScroll.Value + " : TGT --> " + target.VerticalScroll.Value);
+            scrolling = false;
+        }
+
+        private void SyncNumToVerb() => ScrollPosChanged(editorNumeric, editorVerbose);
+
+        private void SyncVerbToNum() => ScrollPosChanged(editorVerbose, editorNumeric);
+
+        private void editorNumeric_Scroll(object sender, ScrollEventArgs e) => SyncVerbToNum();
+
+        private void editorNumeric_SelectionChangedDelayed(object sender, EventArgs e) => SyncVerbToNum();
+
+        private void editorNumeric_SelectionChanged(object sender, EventArgs e) => SyncVerbToNum();
+
+        private void editorVerbose_Scroll(object sender, ScrollEventArgs e) => SyncNumToVerb();
+
+        private void editorVerbose_SelectionChanged(object sender, EventArgs e) => SyncNumToVerb();
+
+        private void editorVerbose_SelectionChangedDelayed(object sender, EventArgs e) => SyncNumToVerb();
+
+        private void Box_KeyDown(object sender, KeyEventArgs e)
+        {
+            FastColoredTextBox box = sender as FastColoredTextBox;
+            if (e.Control && e.KeyCode == Keys.F)
+            {
+                box.ShowFindDialog();
+                e.SuppressKeyPress = true;
+                e.Handled = true;
+            } else if (e.Control && e.KeyCode == Keys.R)
+            {
+                box.ShowReplaceDialog();
+                e.SuppressKeyPress = true;
+                e.Handled = true;
+            }
+        }
+    }
+}
